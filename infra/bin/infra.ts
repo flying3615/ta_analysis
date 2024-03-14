a6a9e570e138ba15446d28fb95ca692144ded34c
Mark Metcalfe <MMetcalfe@linz.govt.nz>
2024-03-14 16:56:18 +1300

    chore: SJ-1047: Update cdk and ci/cd definitions

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,35 +1,41 @@
-import * as cdk from 'aws-cdk-lib'
-import { AwsAccounts, AwsEnv, LinzAccountName } from '@linz/accounts'
-import { StaticSiteBucketStack } from '@linz/linz-static-site'
-import { StepGithubCIRoleStack } from '@linz/step-utils'
+import { AwsAccounts, AwsEnv, LinzAccountName } from '@linz/accounts';
+import { StaticSiteBucketStack } from '@linz/linz-static-site';
+import { StepGithubCIRoleStack } from '@linz/step-utils';
+import * as cdk from 'aws-cdk-lib';
 
-const app = new cdk.App()
-
-const config = {
-  kartoffel: {
-    environmentName: 'nonprod',
-    environmentType: AwsEnv.NonProduction,
-  }
-}
-
-Object.values(config).map((values) => {
-  // Create new s3 buckets for hosting site - https://github.com/linz/lucl/tree/master/packages/linz-static-site
-  //  This will also create an AWS role used for deploying to the bucket from github actions.
-  new StaticSiteBucketStack(app, `landonline-survey-plangen-bucket-${values.environmentName}`, {
-    githubRepo: 'linz/landonline-survey-plan-generation',
-    bucketName: `linz-landonline-survey-plangen-${values.environmentName}`
-  })
-})
+const app = new cdk.App();
 
 new StepGithubCIRoleStack(app, 'landonline-survey-plangen-ci-role', {
   applicationName: 'survey-plangen-app',
   env: {
-    account: AwsAccounts.byNameEnv(
-      LinzAccountName.StepSurvey,
-      AwsEnv.NonProduction
-    ).id,
-    region: 'ap-southeast-2'
+    account: AwsAccounts.byNameEnv(LinzAccountName.StepSurvey, AwsEnv.NonProduction).id,
+    region: 'ap-southeast-2',
   },
-  githubRepo: 'linz/landonline-survey-plan-generation',
-  pushRepos: ['step/survey/step-landonline-web-survey-plan-generation-fe'],
-})
+  githubRepo: 'linz/landonline-survey-plangen',
+  pushRepos: ['step/survey/step-landonline-web-survey-plangen-fe'],
+});
+
+const config = {
+  nonprod: {
+    environmentName: 'nonprod',
+    environmentType: AwsEnv.NonProduction,
+  },
+  preprod: {
+    environmentName: 'preprod',
+    environmentType: AwsEnv.PreProduction,
+  },
+  prod: {
+    environmentName: 'prod',
+    environmentType: AwsEnv.Production,
+  },
+};
+
+Object.values(config).map((values) => {
+  // Create new s3 buckets for hosting site - https://github.com/linz/lucl/tree/master/packages/linz-static-site
+  //  This will also create an AWS role used for deploying to the bucket from github actions.
+  new StaticSiteBucketStack(app, `landonline-survey-plangen-${values.environmentName}`, {
+    githubRepo: 'linz/landonline-survey-plangen',
+    bucketName: `linz-landonline-survey-plangen-${values.environmentName}`,
+    websiteErrorDocument: 'plan-generation/index.html',
+  });
+});
