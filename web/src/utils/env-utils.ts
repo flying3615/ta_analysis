c9156e14b1c9cdd1a26f089e9ea48bcffdb25ed3
Tim Kung <tkung@linz.govt.nz>
2024-04-10 16:47:12 +1200

    feat: SJ-1043: adding env specific static config

--- web/src/utils/env-utils.ts
+++ web/src/utils/env-utils.ts
@@ -1,4 +1,6 @@
 export async function initializeEnv(): Promise<void> {
+  const { splitKey, oidcIssuer } = await fetch("/plan-generation/config/env.json").then((r) => r.json());
+
   // Add to window, so we can check in console the current build versions
   try {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -6,4 +8,9 @@ export async function initializeEnv(): Promise<void> {
   } catch (_ex) {
     // ignore
   }
+
+  window._env_ = {
+    splitKey,
+    oidcIssuer,
+  };
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,25 +1,45 @@
 import react from "@vitejs/plugin-react";
 import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
 import { defineConfig } from "vite";
 
 const commitHash = execSync("git rev-parse --short HEAD").toString();
 const commitDate = execSync("git log -1 HEAD --format=%cI").toString();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-    assetsDir: "public",
-    sourcemap: true,
-    target: "esnext",
-  },
-  define: {
-    "process.env": process.env,
-    __BUILDDETAIL__: JSON.stringify({ buildVersion: commitHash.trim(), buildTimestamp: commitDate.trim() }),
-  },
-  base: "/plan-generation",
-  server: {
-    port: 11065,
-  },
+export default defineConfig(({ mode }) => {
+  const isProdMode = mode === "production";
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "build",
+      assetsDir: "public",
+      sourcemap: true,
+      target: "esnext",
+    },
+    define: {
+      "process.env": process.env,
+      __BUILDDETAIL__: JSON.stringify({ buildVersion: commitHash.trim(), buildTimestamp: commitDate.trim() }),
+    },
+    base: isProdMode ? "/plan-generation/" : "/",
+    server: {
+      port: 11065,
+      proxy: {
+        "/plan-generation/config/": {
+          target: "http://localhost:11065",
+          selfHandleResponse: true,
+          rewrite: (path) => path.replace("/plan-generation/config/", ""),
+          bypass: (req, res) => {
+            const requestedFileName = encodeURIComponent(req.url!.slice("/plan-generation/config/".length));
+            const localFilePath = path.join(__dirname, "config/local", requestedFileName);
+            console.log(`proxying ${req.url} to ${localFilePath}`);
+            const data = fs.readFileSync(localFilePath, "utf-8");
+            res.end(data);
+          },
+        },
+      },
+    },
+  }
 });
