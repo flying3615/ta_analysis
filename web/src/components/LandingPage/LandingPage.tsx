aeacb9c1ebd6a87b4e0afb01f5a039454016ab50
Tim Kung <tkung@linz.govt.nz>
2024-04-11 09:08:59 +1200

    fix: SJ-1043: adding .env config for injecting vite variables

--- web/src/components/LandingPage/LandingPage.tsx
+++ web/src/components/LandingPage/LandingPage.tsx
@@ -3,10 +3,7 @@ import "./LandingPage.scss";
 function LandingPage() {
   return (
     <div className="LandingPage">
-      <img
-        src="/plan-generation/static/media/LINZ_Logo_coloured.svg"
-        alt="Toitū Te Whenua Land Information New Zealand"
-      />
+      <img src="/static/media/LINZ_Logo_coloured.svg" alt="Toitū Te Whenua Land Information New Zealand" />
       <h1>Landonline Survey Plan Generation</h1>
       <p>Coming soon</p>
     </div>
