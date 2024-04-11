8c7838cfcbcbcf76a20e552cdc61cd929f2a4b5b
Tim Kung <tkung@linz.govt.nz>
2024-04-12 10:23:23 +1200

    fix: SJ-1043: removing dynamic base path for vite config to keep consistent across envs

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,41 +9,37 @@ const commitHash = execSync("git rev-parse --short HEAD").toString();
 const commitDate = execSync("git log -1 HEAD --format=%cI").toString();
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProdMode = mode === "production";
-
-  return {
-    plugins: [react()],
-    build: {
-      outDir: "build",
-      assetsDir: "public",
-      sourcemap: true,
-      target: "esnext",
-    },
-    define: {
-      "process.env": process.env,
-      __BUILDDETAIL__: JSON.stringify({ buildVersion: commitHash.trim(), buildTimestamp: commitDate.trim() }),
-    },
-    resolve: {
-      alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
-    },
-    base: isProdMode ? "/plan-generation/" : "/",
-    server: {
-      port: 11065,
-      proxy: {
-        "/plan-generation/config/": {
-          target: "http://localhost:11065",
-          selfHandleResponse: true,
-          rewrite: (path) => path.replace("/plan-generation/config/", ""),
-          bypass: (req, res) => {
-            const requestedFileName = encodeURIComponent(req.url!.slice("/plan-generation/config/".length));
-            const localFilePath = path.join(__dirname, "config/local", requestedFileName);
-            console.log(`proxying ${req.url} to ${localFilePath}`);
-            const data = fs.readFileSync(localFilePath, "utf-8");
-            res.end(data);
-          },
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "build",
+    assetsDir: "public",
+    sourcemap: true,
+    target: "esnext",
+  },
+  define: {
+    "process.env": process.env,
+    __BUILDDETAIL__: JSON.stringify({ buildVersion: commitHash.trim(), buildTimestamp: commitDate.trim() }),
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
+  },
+  base: "/plan-generation/",
+  server: {
+    port: 11065,
+    proxy: {
+      "/plan-generation/config/": {
+        target: "http://localhost:11065",
+        selfHandleResponse: true,
+        rewrite: (path) => path.replace("/plan-generation/config/", ""),
+        bypass: (req, res) => {
+          const requestedFileName = encodeURIComponent(req.url!.slice("/plan-generation/config/".length));
+          const localFilePath = path.join(__dirname, "config/local", requestedFileName);
+          console.log(`proxying ${req.url} to ${localFilePath}`);
+          const data = fs.readFileSync(localFilePath, "utf-8");
+          res.end(data);
         },
       },
     },
-  };
+  },
 });
