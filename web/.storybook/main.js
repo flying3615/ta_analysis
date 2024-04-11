2442856d1fde00fc5cd441771bdd35bc07036c0f
Mark Metcalfe <MMetcalfe@linz.govt.nz>
2024-04-12 10:34:01 +1200

    chore: SJ-1047: Upgrade storybook to v8

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -3,15 +3,14 @@ export const stories = ["../src/**/*.stories.@(js|jsx|ts|tsx)"];
 export const addons = [
   "@storybook/addon-links",
   "@storybook/addon-essentials",
-  "@storybook/addon-interactions"
+  "@storybook/addon-interactions",
+  "@chromatic-com/storybook"
 ];
 export const framework = {
   name: "@storybook/react-vite",
   options: {},
 };
-export const features = {
-  storyStoreV7: true,
-};
+export const features = {};
 export const staticDirs = ["../public", { from: "../public", to: "/plan-generation" }];
 export async function viteFinal(config) {
   return {
