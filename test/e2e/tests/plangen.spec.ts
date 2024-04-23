901d307bd7b1b7335057e9ac828609b3e4346d1f
KTyagiLinz <KTyagi@linz.govt.nz>
2024-04-23 12:58:14 +1200

    SJ-1044 removed pw mock

--- test/e2e/tests/plangen.spec.ts
+++ test/e2e/tests/plangen.spec.ts
@@ -5,29 +5,6 @@ import { APP_URL, TEST_PASSWORD, TEST_USERNAME } from "../playwright.config";
 test.describe("plangen", () => {
   test.setTimeout(60000);
   test("has header", async ({ page }) => {
-    // TODO: Mock the api call before navigating as currently app is not redirecting to diagram page after entering login credential
-    await page.route("**/v1/my-accesses", async (route) => {
-      const responseMock = {
-        id: "extsurv1",
-        loginType: "EXTN",
-        roles: ["CATEGORY_EXSU", "CATEGORY_SPIL"],
-        profiles: ["PROFILE_EXTERNAL_SURVEY_USER"],
-        firms: [
-          {
-            id: "firm4",
-            name: "Firm 4",
-            privileges: [
-              "PRV_CREATE_SURVEY",
-              "PRV_PRE_VALIDATE",
-              "PRV_SURVEY_SIGN_SUBMIT",
-              "PRV_VIEW_SURVEY",
-            ],
-          },
-        ],
-      };
-      await route.fulfill({ status: 200, json: responseMock });
-    });
-
     await page.goto(APP_URL);
     await page.waitForURL(/\/realms\/landonline/, { timeout: 40000 });
     await page.fill('[data-testid="userID"]', TEST_USERNAME, {
