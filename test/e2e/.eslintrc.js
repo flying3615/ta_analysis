077c9494a6aa74f98733889d7487011f3da982cb
KTyagiLinz <KTyagi@linz.govt.nz>
2024-04-24 17:12:27 +1200

    SJ-1044 updated pw eslintrc and included prettier

--- test/e2e/.eslintrc.js
+++ test/e2e/.eslintrc.js
@@ -1,34 +1,31 @@
 let extend = require("@linzjs/style/.eslintrc.cjs");
-
-extend.extends.push("plugin:playwright/playwright-test");
-extend.rules = {
-  ...extend.rules,
-  ...{
-    "lines-between-class-members": [
-      "error",
-      { enforce: [{ blankLine: "always", prev: "method", next: "method" }] },
-    ],
-    "playwright/missing-playwright-await": "warn",
-    "playwright/no-page-pause": "warn",
-    "playwright/no-element-handle": "off",
-    "playwright/no-eval": "off",
-    "playwright/no-focused-test": "warn",
-    "playwright/no-skipped-test": "off",
-    "playwright/no-wait-for-timeout": "off",
-    "playwright/no-force-option": "off",
-    "playwright/max-nested-describe": "off",
-    "playwright/no-conditional-in-test": "off",
-    "playwright/no-useless-not": "off",
-    "playwright/no-restricted-matchers": "off",
-    "playwright/prefer-lowercase-title": "off",
-    "playwright/prefer-to-have-length": "off",
-    "playwright/require-top-level-describe": "error",
-    "playwright/valid-expect": "off",
-    "playwright/expect-expect": "off",
-    "playwright/no-networkidle": "off",
-    "playwright/no-wait-for-selector": "off",
-  },
+module.exports = {
+  ...extend,
+  ...extend.overrides.push({
+    files: "tests/**",
+    extends: "plugin:playwright/recommended",
+    rules: {
+      "lines-between-class-members": ["error", { enforce: [{ blankLine: "always", prev: "method", next: "method" }] }],
+      "playwright/missing-playwright-await": "warn",
+      "playwright/no-page-pause": "warn",
+      "playwright/no-element-handle": "off",
+      "playwright/no-eval": "off",
+      "playwright/no-focused-test": "warn",
+      "playwright/no-skipped-test": "off",
+      "playwright/no-wait-for-timeout": "off",
+      "playwright/no-force-option": "off",
+      "playwright/max-nested-describe": "off",
+      "playwright/no-conditional-in-test": "off",
+      "playwright/no-useless-not": "off",
+      "playwright/no-restricted-matchers": "off",
+      "playwright/prefer-lowercase-title": "off",
+      "playwright/prefer-to-have-length": "off",
+      "playwright/require-top-level-describe": "error",
+      "playwright/valid-expect": "off",
+      "playwright/expect-expect": "off",
+      "playwright/no-networkidle": "off",
+      "playwright/no-wait-for-selector": "off",
+    },
+  }),
+  ignorePatterns: ["test-results/*", "output/*"],
 };
-extend.ignorePatterns = ["test-results/*", "output/*"];
-
-module.exports = extend;
--- test/e2e/tests/plangen.spec.ts
+++ test/e2e/tests/plangen.spec.ts
@@ -3,8 +3,6 @@ import { expect, test } from "@playwright/test";
 test.describe("plangen", () => {
   test("has header", async ({ page }) => {
     await page.goto("plan-generation/");
-    await expect(
-      page.getByRole("heading", { name: "Plan generation" }),
-    ).toBeVisible();
+    await expect(page.getByRole("heading", { name: "Plan generation" })).toBeVisible();
   });
 });
