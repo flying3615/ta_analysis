2d778ee8e40a97efcce76ac4fb9ccc1b72088efa
Tim Kung <tkung@linz.govt.nz>
2024-05-09 14:42:15 +1200

    fix: SJ-1146: lint fix

--- web/src/mocks/mockHandlers.ts
+++ web/src/mocks/mockHandlers.ts
@@ -2,10 +2,12 @@ import { rest } from "msw";
 import { mockOriginMark } from "./mockSurveyFeatures";
 
 export const handlers = [
-  rest.get(/\/survey-features\/123/, (_, res, ctx) => res(
-    ctx.status(200, "OK"),
-    ctx.json({
-      marks: [mockOriginMark(1)],
-    }),
-  )),
+  rest.get(/\/survey-features\/123/, (_, res, ctx) =>
+    res(
+      ctx.status(200, "OK"),
+      ctx.json({
+        marks: [mockOriginMark(1)],
+      }),
+    ),
+  ),
 ];
