b3d65ce9b5722bc0e59831bd9a3c3498fff9a89b
Tim Kung <tkung@linz.govt.nz>
2024-05-09 15:57:47 +1200

    fix: SJ-1146: add define diagrams unit tests

--- web/src/components/DefineDiagrams/__tests__/DefineDiagrams.test.tsx
+++ web/src/components/DefineDiagrams/__tests__/DefineDiagrams.test.tsx
@@ -1,29 +1,64 @@
 import { screen, waitFor } from "@testing-library/react";
 import { DefineDiagrams } from "@/components/DefineDiagrams/DefineDiagrams.tsx";
 import { getMockMap, LayerType } from "@linzjs/landonline-openlayers-map";
-import { BASEMAP_LAYER_NAME } from "@/components/DefineDiagrams/MapLayers.ts";
-import { MemoryRouter, generatePath } from "react-router-dom";
+import { BASEMAP_LAYER_NAME, MARKS_LAYER_NAME } from "@/components/DefineDiagrams/MapLayers.ts";
+import { MemoryRouter, Route, Routes, generatePath } from "react-router-dom";
 import { Paths } from "@/Paths";
 import { renderWithProviders } from "@/test-utils/jest-utils";
 
 describe("DefineDiagrams", () => {
-  it("should render", async () => {
-    // We need this *after* createObjectURL* gets stubbed
-    getMockMap().resetState();
+  const renderDefineDiagrams = (transactionId: number = 123) => {
+    const path = generatePath(Paths.defineDiagrams, { transactionId });
 
     renderWithProviders(
-      <MemoryRouter initialEntries={[`${generatePath(Paths.defineDiagrams, { transactionId: 123 })}`]}>
-        <DefineDiagrams mock={true} />
+      <MemoryRouter initialEntries={[path]}>
+        <Routes>
+          <Route path={Paths.defineDiagrams} element={<DefineDiagrams mock={true} />} />
+        </Routes>
       </MemoryRouter>,
     );
+  };
 
+  beforeEach(() => {
+    // We need this *after* createObjectURL* gets stubbed
+    getMockMap().resetState();
+  });
+
+  it("should render", async () => {
+    renderDefineDiagrams();
+
+    // loading spinner while fetching features
+    await screen.findByTestId("loading-spinner");
+
+    // openlayers map and it's layers should render after features fetched
     const mockMap = getMockMap();
-
     await waitFor(() => {
       expect(mockMap).toHaveLayer(BASEMAP_LAYER_NAME, LayerType.TILE);
     });
 
-    // header toggle label
+    // header toggle label is visible
     expect(screen.getByText("Diagrams")).toBeInTheDocument();
+  });
+
+  it("should show marks on the map", async () => {
+    renderDefineDiagrams();
+
+    // openlayers map and it's layers should render
+    const mockMap = getMockMap();
+    await waitFor(() => {
+      expect(mockMap).toHaveLayer(MARKS_LAYER_NAME, LayerType.VECTOR);
+    });
+
+    // validate marks visible
+    const marksLayerState = mockMap.layerState[MARKS_LAYER_NAME];
+    await waitFor(() => expect(marksLayerState?.visible).toBeTruthy());
+    await waitFor(() => expect(marksLayerState).toHaveFeatureCount(3));
+    expect(marksLayerState).toHaveFeature(1);
+  });
+
+  it("displays error when survey not found", async () => {
+    renderDefineDiagrams(404);
+
+    expect(screen.getByText("Sorry, there was an error")).toBeInTheDocument();
   });
 });
--- web/src/mocks/mockHandlers.ts
+++ web/src/mocks/mockHandlers.ts
@@ -6,8 +6,13 @@ export const handlers = [
     res(
       ctx.status(200, "OK"),
       ctx.json({
-        marks: [mockOriginMark(1)],
+        marks: [mockOriginMark(1), mockOriginMark(2), mockOriginMark(3)],
       }),
     ),
+  ),
+
+  // 404 case
+  rest.get(/\/survey-features\/404/, (_, res, ctx) =>
+    res(ctx.status(404, "Not found"), ctx.json({ code: 404, message: "Not found" })),
   ),
 ];
