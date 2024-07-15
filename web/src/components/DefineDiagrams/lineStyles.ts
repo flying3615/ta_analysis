f5bce9484837373ac9c694cf479ca08e17c6ca99
Greg Finch <102272488+gregfinchlinz@users.noreply.github.com>
2024-07-15 15:03:01 +1200

    fix: SRVPUW-833 Updated style for abuttal line (#109)
    * Updated styles

--- web/src/components/DefineDiagrams/lineStyles.ts
+++ web/src/components/DefineDiagrams/lineStyles.ts
@@ -4,9 +4,10 @@ import { Style } from "ol/style";
 import Stroke from "ol/style/Stroke";
 
 export const lineStyles = (feature: FeatureLike): Style => {
-  const color = "#000000";
+  const symbolType = feature.get("symbolType");
+  const color = symbolType === LinesSymbolType.CT ? "#000000" : "#878787";
   //line dash array goes [dash, gap, dash, gap]
-  const lineDash = feature.get("symbolType") === LinesSymbolType.CT ? [8, 4, 2, 4] : [5, 5];
+  const lineDash = symbolType === LinesSymbolType.CT ? [8, 6, 2, 6] : [2, 7];
 
   return new Style({
     stroke: new Stroke({ width: 1.5, lineDash, color }),
