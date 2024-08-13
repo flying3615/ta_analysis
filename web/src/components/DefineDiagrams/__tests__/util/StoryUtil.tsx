dbb58c629eaf17d5b3ce0bebf3c792e03af72965
aasthasanghi <19764610+aasthasanghi@users.noreply.github.com>
2024-08-13 16:51:00 +1200

    test: SRVPUW-759 fixing Storyutil (#176)

--- web/src/components/DefineDiagrams/__tests__/util/StoryUtil.tsx
+++ web/src/components/DefineDiagrams/__tests__/util/StoryUtil.tsx
@@ -5,25 +5,24 @@ import { Coordinate } from "ol/coordinate";
 import { sleep } from "@/test-utils/storybook-utils";
 
 export const drawOnMap = async (coordinatesToClick: Coordinate[]) => {
-  // eslint-disable-next-line testing-library/no-node-access
-  const viewport = document.querySelector(".ol-viewport");
   /* eslint-disable */
   const map = (window as any).map as Map;
+  const viewport = map.getViewport();
 
   if (!viewport || !map) {
     console.log("Viewport or map not defined", { viewport, map });
     return;
   }
 
+  const domRect = viewport.getBoundingClientRect();
   for (const coord of coordinatesToClick) {
     const pixel = map.getPixelFromCoordinate(coord);
-
     await userEvent.pointer({
       keys: "[MouseLeft]",
       target: viewport,
       coords: {
-        clientX: pixel[0],
-        clientY: pixel[1]!+56, //get pixel is currently broken hence we need to add 56 px to click at correct place
+        clientX: (pixel[0]??0) + domRect.x,
+        clientY: (pixel[1]??0) + domRect.y,
       },
     });
     // set a delay between each click on map so we can see the clicks happening
