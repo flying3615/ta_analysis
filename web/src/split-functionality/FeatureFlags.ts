7bed196eadec533ed0aa8e7538446095552f2f82
matttdawson <89495499+matttdawson@users.noreply.github.com>
2024-11-18 10:24:38 +1300

    feat: SRVPUW-1120 plan generation auto recovery split (#511)
    SRVPUW-1120 plan generation auto recovery split

--- web/src/split-functionality/FeatureFlags.ts
+++ web/src/split-functionality/FeatureFlags.ts
@@ -6,6 +6,7 @@ export enum FEATUREFLAGS {
   SURVEY_PLAN_GENERATION_PREVIEW_COMPILATION = "survey-plan-generation-preview-compilation",
   SURVEY_PLAN_GENERATION_LABEL_PREFERENCES = "survey-plan-generation-label-preferences",
   SURVEY_PLAN_GENERATION_MAINTAIN_DIAGRAM_LAYERS = "survey-plan-generation-maintain-diagram-layers",
+  SURVEY_PLAN_GENERATION_AUTO_RECOVERY = "survey-plan-generation-auto-recovery",
 }
 
 export enum TREATMENTS {
