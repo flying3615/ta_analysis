730ac7d3cca790ce47706b557386c56e6afdede2
Ezy <ekeddell@linz.govt.nz>
2024-11-28 14:30:36 +1300

    feat: SRVPUW-1092 add feedback url to feedback button (#580)
    add feedback url to plangen

--- web/src/components/CommonButtons.tsx
+++ web/src/components/CommonButtons.tsx
@@ -4,9 +4,10 @@ import { ActionHeaderButton } from "@/components/Header/ActionHeaderButton";
 import { getHelpUrl } from "@/util/httpUtil";
 
 export const CommonButtons = () => {
+  const feedbackUrl = "https://forms.office.com/r/db204DXw62";
   return (
     <>
-      <ActionHeaderButton title="Feedback" icon="ic_feedback_lightbulb_alt" onClick={() => alert("Not implemented")} />
+      <ActionHeaderButton title="Feedback" icon="ic_feedback_lightbulb_alt" href={feedbackUrl} openInNewTab={true} />
       <ActionHeaderButton title="Help" icon="ic_help_outline" href={getHelpUrl()} openInNewTab={true} />
     </>
   );
